6df1196dd857f1285cbc83888f4a35f5f8357552
Markhehe <doodsmew@gmail.com>
2025-04-22 00:09:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("openMe").addEventListener("click", openMe);
+function openMe(){
+    window.open("window.html", "newWindow", "width=1000,height=900");
+}
+
+
+
+
---
+++ script2.js
@@ -0,0 +1,9 @@
+document.getElementById("clickMe").addEventListener("click", mabuhay);
+function mabuhay(){
+    document.getElementById("mabuhay").innerHTML = "MABUHAY!"
+}
+
+document.getElementById("quit").addEventListener("click", quit);
+function quit(){
+    window.close();
+}
